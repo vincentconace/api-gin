9e70b1095cea031017795641f9381b6c101845c7
vincentconace <conacevincent@gmail.com>
2022-05-23 19:51:29 -0300

    Add package redis

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -2,18 +2,58 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
+type Redis interface {
+	Set(ctx context.Context, key string, data interface{}, timeDuration time.Duration) (string, error)
+	Get(ctx context.Context, key string) (interface{}, error)
+}
+
+type redisClient struct {
+	rd *redis.Client
+}
+
 var ctx = context.Background()
 
-func RedisClient() *redis.Client {
+func NewRedisClient() Redis {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	return rdb
+	return &redisClient{rd: rdb}
+}
+
+func (r *redisClient) Set(ctx context.Context, key string, data interface{}, timeDuration time.Duration) (string, error) {
+	dataByte, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	datastring, err := r.rd.Set(ctx, key, string(dataByte), timeDuration*time.Hour).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return datastring, err
+}
+
+func (r *redisClient) Get(ctx context.Context, key string) (interface{}, error) {
+	var data interface{}
+	dataByte, err := r.rd.Get(ctx, key).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal([]byte(dataByte), &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return data, err
 }
